5397beb6a6fba464b89df7abd5375aae7510f565
hamzaammi7e9-ux <hamza.ammi.7e9@itb.cat>
2025-10-03 08:15:20 +0200

    v2.1

--- src/ITB-02b.3.kt
+++ src/ITB-02b.3.kt
@@ -1,17 +1,20 @@
 import java.util.Scanner
-import javax.swing.text.Segment
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    //Llegim el numero del dia
+    //Llegim els cinc nombres
     val primerNombre = scanner.nextInt()
     val segonNombre = scanner.nextInt()
     val tercerNombre = scanner.nextInt()
     val quartNombre = scanner.nextInt()
     val cinqueNombre = scanner.nextInt()
+
+    //Fem un condicional que mostri "SI" si el primer nombre es més petit que el segon, el segon més gran que el tercer...
     if (primerNombre < segonNombre && tercerNombre < segonNombre && quartNombre > tercerNombre && cinqueNombre < quartNombre) {
         print("SI")
+    //I si no es compleix la primera condició, que mostri "SI" si el primer es més gran que el segon, el segon més petit que el tercer...
     } else if (primerNombre > segonNombre && segonNombre < tercerNombre && quartNombre < tercerNombre && quartNombre < cinqueNombre) {
-
-    }
+        print("SI")
+    //I si no es compleix cap condició, mostrar "NO"
+    } else print("NO")
 }
