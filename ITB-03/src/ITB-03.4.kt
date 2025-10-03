5c31b8a74a521c4fa753392a4c2f3317e1462bff
hamzaammi7e9-ux <hamza.ammi.7e9@itb.cat>
2025-10-03 10:03:35 +0200

    v2.3

--- ITB-03/src/ITB-03.4.kt
+++ ITB-03/src/ITB-03.4.kt
@@ -2,26 +2,27 @@ import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    //Llegir els 3 nombres
+    //Llegir les coordenades
     val coordenadaX: Int = scanner.nextInt()
     val coordenadaY: Int = scanner.nextInt()
+    //Creem una variable per a cada quadrant
     val quadrant1: String = "1"
     val quadrant2: String = "2"
     val quadrant3: String = "3"
     val quadrant4: String = "4"
-    val
-    when{
+
+    //Creem condicions per a cada cas depenent de les coordenades introduïdes i mostrem a quin o quins quadrants es troba.
+    when {
         coordenadaY > 0 && coordenadaX > 0 -> print(quadrant1)
-        coordenadaY <
+        coordenadaY < 0 && coordenadaX < 0 -> print(quadrant3)
+        coordenadaY > 0 && coordenadaX < 0 -> print(quadrant2)
+        coordenadaY < 0 && coordenadaX > 0 -> print(quadrant4)
+        coordenadaX == 0 && coordenadaY > 0 -> print("$quadrant1,$quadrant2")
+        coordenadaX < 0 && coordenadaY == 0 -> print("$quadrant2,$quadrant3")
+        coordenadaY < 0 && coordenadaX == 0 -> print("$quadrant3,$quadrant4")
+        coordenadaY == 0 && coordenadaX > 0 -> print("$quadrant1,$quadrant4")
+        coordenadaY == 0 && coordenadaX == 0 -> print("1,2,3,4")
     }
-
-
-
-        print(resultat)
-
-
-
-
 
 
 }
---
+++ ITB-04/src/ITB-04.3.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    //Llegim les dades
+    val hora = scanner.nextInt()
+    val minuts = scanner.nextInt()
+    val segons = scanner.nextInt()
+    //Creem un condicional que comprovi si l'hora, els minuts i els segons estan en l'interval correcte i mostri "SI"
+    //Si no, mostrem "NO"
+    when{
+        hora in 0 until 24 && minuts in 0..59 && segons in 0..59-> print ("SI")
+        else -> print("NO")
+    }
+}
