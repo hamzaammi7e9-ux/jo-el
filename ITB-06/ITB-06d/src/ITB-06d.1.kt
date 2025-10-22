c9cea92a915480e026e8eeff653988c6f4d52639
hamza.ammi.7e9-ux <hamza.ammi.7e9@itb.cat>
2025-10-22 10:45:33 +0200

    6d

---
+++ ITB-06/ITB-06d/src/ITB-06d.1.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    //Llegim els casos totals
+    var casosTotals = scanner.nextInt()
+    scanner.nextLine()
+
+    repeat(casosTotals){
+        val frase = scanner.nextLine()
+        for()
+    }
+}
+
+
+
