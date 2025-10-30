6ce3aa5d24c1f700be78ddfbf2ff6a6bc96dc891
hamzaammi7e9-ux <hamza.ammi.7e9@itb.cat>
2025-10-30 10:45:16 +0100

    ITB-07a acabado

---
+++ ITB-07/ITB-07b/src/ITB-07b.1.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+fun main() {
+    //Input:
+    // — Read the amount iterations the user wants to do.
+    // — Each iteration contains:
+    //   — A line with the size of the array of shoes.
+    //   — Another line of K numbers representing the shoes sizes (All of them between 8 and 60)
+    //   — Lastly, a number M, representing the user shoe size.
+//Output:
+    // — Depending on the user input:
+    //   — Print "SI" if there is a shoe size in the array that fits well for the user's.
+    //     (Only when it is either equal to M, one less or one more)
+    //
+    //   — Print "NO" if there is no shoe that meets those requirements.
+
+}
